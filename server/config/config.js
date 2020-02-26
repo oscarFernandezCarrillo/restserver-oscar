3b9f04c33cd56cc10b9a6e86b993dcd488a1bf58
alpha_oscar <oscar.fernandez@alphacredit.mx>
2020-02-25 18:17:05 -0600

    editado config.js

--- server/config/config.js
+++ server/config/config.js
@@ -14,11 +14,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //---------Base de Datos----------
 //================================
 let urlDB;
-// if( process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// }else{
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
     urlDB = 'mongodb+srv://atlas-admin:MCrSU6FoPNvsw4K3@cluster0-q0xls.mongodb.net/cafe?retryWrites=true&w=majority';
 //     //urlDB = 'mongodb+srv://atlas-admin:MCrSU6FoPNvsw4K3@cluster0-q0xls.mongodb.net/cafe';
-// }
+}
 
 process.env.URLDB = urlDB
